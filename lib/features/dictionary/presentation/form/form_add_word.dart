09668ef3bd6d695954fb5cd1d4ae36836acb05b0
Vladislav <Vladislav>
2024-04-11 21:13:26 +0300

    edit form add word

--- lib/features/dictionary/presentation/form/form_add_word.dart
+++ lib/features/dictionary/presentation/form/form_add_word.dart
@@ -9,23 +9,126 @@ class WordFormAddPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Form Add new word"),
+        title: Text("Add new word"),
       ),
       body: Container(
         child: Center(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "Form add new word",
-                style: TextStyle(fontSize: 28, color: Colors.black),
-              ),
-              Icon(
-                Icons.assistant_photo,
-                size: 48,
-                color: Colors.black,
-              )
-            ],
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 4.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: "Word",
+                      labelStyle: TextStyle(
+                        color: Colors.grey[500],
+                      )
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 4.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: "Translation",
+                        labelStyle: TextStyle(
+                            color: Colors.grey[500]
+                        )
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 4.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: "Example",
+                        labelStyle: TextStyle(
+                            color: Colors.grey[500]
+                        )
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 4.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: "Translation example",
+                        labelStyle: TextStyle(
+                            color: Colors.grey[500]
+                        )
+                    ),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(4.0),
+                        child: TextField(
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: "Choose image",
+                          ),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(4.0),
+                        child: TextField(
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: "Choose list",
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Expanded(
+                  flex: 4,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      FilledButton.tonal(
+                        onPressed: () {
+                          print("Cancel button form add");
+                          Navigator.pop(context, true);
+                        },
+                        child: const Text('Cancel'),
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all(
+                              Theme.of(context).colorScheme.primaryContainer),
+                        ),
+                      ),
+                      FilledButton.tonal(
+                        onPressed: () {
+                          print("Add button form add");
+                          Navigator.pop(context, true);
+
+                          /*
+                          Сюди додати метод додавання в базу данних теку
+                           */
+                        },
+                        child: const Text('Add'),
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all(
+                              Theme.of(context).colorScheme.primaryContainer),
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
